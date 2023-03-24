56b1bc7abdab0be685c234f3cb8390b5f614f07f
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-03-24 13:28:21 +1300

    Add customer class

---
+++ BookingApp/Customers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp
+{
+    internal class Customers
+    {
+        private string name;
+        private string address;
+        private int number;
+        public string email;
+
+        public Customers(string _name, string _address, int _number, string _email)
+        {
+            name = _name;
+            address = _address;
+            number = _number;
+            email = _email;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
