742a3c73b0eae1087b8e1e61689177d866103bd0
zilazach <105255994+zilazach@users.noreply.github.com>
2023-03-24 14:34:23 +1300

    add menu

---
+++ BookingApp/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp
+{
+    internal class Menu
+    {
+        static void menu()
+        {
+            switch(Console.ReadLine()) 
+            {
+                case "1":
+
+                break;
+                case "2":
+
+                break;
+                case "3":
+
+                break;
+                case "4":
+
+                 break;
+            }
+        }
+    }
+}
