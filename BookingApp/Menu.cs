ddefa2d189bab7bd9a5bb24197e5bb648dab21fa
zilazach <105255994+zilazach@users.noreply.github.com>
2023-03-31 13:15:51 +1300

    add menu

--- BookingApp/Menu.cs
+++ BookingApp/Menu.cs
@@ -10,19 +10,20 @@ namespace BookingApp
     {
         static void menu()
         {
+            Console.WriteLine("1:tour available\n2:book tour\n3:add tour\n4:cancel tour");
             switch(Console.ReadLine()) 
             {
                 case "1":
-
+                    Console.WriteLine("tour 1 3 5 are available");
                 break;
                 case "2":
-
+                    Console.WriteLine(" 2 or 4");
                 break;
                 case "3":
-
+                    Console.WriteLine();
                 break;
                 case "4":
-
+                    Console.WriteLine("1 2 3 4 5");
                  break;
             }
         }
